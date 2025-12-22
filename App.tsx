7fdd3aaf03d15b7b2fb2821e93621a925dd81638
VisionManagers <sukhneet@visionmanagers.com>
2025-12-22 14:54:05 -0800

    Update App.tsx
    Revert pages to folder structure

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
-import Home from './Home';
-import StartHere from './StartHere';
-import Solutions from './Solutions';
-import TheLab from './TheLab';
-import Insights from './Insights';
-import SovereignIntelligence from './insights/SovereignIntelligence';
-import LegalAIDeployment from './insights/LegalAIDeployment';
-import BottlenecksROI from './insights/BottlenecksROI';
-import Contact from './Contact';
-import PrivacyPolicy from './PrivacyPolicy';
-import About from './About';
+import Home from './pages/Home';
+import StartHere from './pages/StartHere';
+import Solutions from './pages/Solutions';
+import TheLab from './pages/TheLab';
+import Insights from './pages/Insights';
+import SovereignIntelligence from './pages/insights/SovereignIntelligence';
+import LegalAIDeployment from './pages/insights/LegalAIDeployment';
+import BottlenecksROI from './pages/insights/BottlenecksROI';
+import Contact from './pages/Contact';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import About from './pages/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FloatingChat from './components/FloatingChat';
